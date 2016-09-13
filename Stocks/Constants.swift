620168fc185e407605ff0872d5be6fbdbc86d19b
Todd Sutter <tsuttert@gmail.com>
2016-09-13 12:20:38 -0500

    started stocks project

---
+++ Stocks/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Stocks
+//
+//  Created by Todd Sutter on 9/13/16.
+//  Copyright © 2016 toddsutter. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+// Put global constants here
+
+let SIZE_IPAD: CGSize = CGSize(width: 1024, height: 768)
---
+++ Stocks/MenuScene.swift
@@ -0,0 +1,23 @@
+//
+//  MenuScene.swift
+//  Stocks
+//
+//  Created by Todd Sutter on 9/13/16.
+//  Copyright (c) 2016 toddsutter. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
